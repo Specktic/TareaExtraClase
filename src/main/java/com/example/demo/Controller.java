ca3c95006697b1b10a51b65ec481618446cdd22b
Specktic <111095859+Specktic@users.noreply.github.com>
2022-08-16 18:22:25 -0600

    Inserta objetos en la tabla a mostrar

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,19 +1,23 @@
 package com.example.demo;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
     @FXML
     private Button SearchButton;
 
@@ -22,6 +26,46 @@ public class Controller {
 
     @FXML
     private TextArea Notas;
+
+    @FXML
+    private TableView<Estudiante> TablaNotas;
+
+    @FXML
+    private TableColumn<Estudiante, Integer> Carnet;
+
+    @FXML
+    private TableColumn<Estudiante, String> Nombre;
+
+    @FXML
+    private TableColumn<Estudiante, String> Correo;
+
+    @FXML
+    private TableColumn<Estudiante, Integer> Telefono;
+
+    @FXML
+    private TableColumn<Estudiante, String> Nick;
+
+    @FXML
+    private TableColumn<Estudiante, Character> Tipo;
+
+    @FXML
+    private TableColumn<Estudiante, Byte> Examenes;
+
+    @FXML
+    private TableColumn<Estudiante, Byte> Quices;
+
+    @FXML
+    private TableColumn<Estudiante, Byte> Tareas;
+
+    @FXML
+    private TableColumn<Estudiante, Byte> Proyecto1;
+
+    @FXML
+    private TableColumn<Estudiante, Byte> Proyecto2;
+
+    @FXML
+    private TableColumn<Estudiante, Byte> Proyecto3;
+
 
     public void SearchButtonEvent(ActionEvent event){
         FileChooser fc = new FileChooser();
@@ -46,13 +90,14 @@ public class Controller {
                     String[] fila = linea.split(",");
 
                     for(String indice : fila) {
-                        System.out.printf("%-20s", indice);
+                        System.out.printf("%s", indice); // printf es un metodo sobrecargado de la clase PrintStream
 
                     }
                     System.out.println();
 
                     Notas.appendText(linea);
                     Notas.appendText("\n");
+                    TablaNotas.setItems(list);
                 }
             }
             catch (Exception e) {
@@ -69,9 +114,27 @@ public class Controller {
             System.out.println("Archivo Invalido");
         }
 
-        }
+    }
 
-        public void TextAreaDisplay() {
+    ObservableList<Estudiante> list = FXCollections.observableArrayList(
+            new Estudiante(8, "Isa", "correo@", 9, "Sam", 'A', (byte) 100,
+                    (byte) 100, (byte) 100, (byte) 100, (byte) 100, (byte) 100)
+    );
 
-        }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Carnet.setCellValueFactory(new PropertyValueFactory<Estudiante, Integer>("Carnet"));
+        Nombre.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("Nombre"));
+        Correo.setCellValueFactory(new PropertyValueFactory<Estudiante, String>("Correo"));
+        Telefono.setCellValueFactory(new PropertyValueFactory<Estudiante, Integer>("Carnet"));
+        Nick.setCellValueFactory(new PropertyValueFactory<Estudiante, String >("Nick"));
+        Tipo.setCellValueFactory(new PropertyValueFactory<Estudiante, Character>("Tipo"));
+        Examenes.setCellValueFactory(new PropertyValueFactory<Estudiante, Byte>("Examenes"));
+        Quices.setCellValueFactory(new PropertyValueFactory<Estudiante, Byte>("Quices"));
+        Tareas.setCellValueFactory(new PropertyValueFactory<Estudiante, Byte>("Tareas"));
+        Proyecto1.setCellValueFactory(new PropertyValueFactory<Estudiante, Byte>("Proyecto1"));
+        Proyecto2.setCellValueFactory(new PropertyValueFactory<Estudiante, Byte>("Proyecto2"));
+        Proyecto3.setCellValueFactory(new PropertyValueFactory<Estudiante, Byte>("Proyecto3"));
+
+    }
 }
